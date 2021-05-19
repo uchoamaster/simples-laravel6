53d733f7035f5f32d6505dd03cc001c6e530066a
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-05-19 16:50:20 -0300

    criando migrations order

--- app/Http/Controllers/Admin/ReportsController.php
+++ app/Http/Controllers/Admin/ReportsController.php
@@ -5,18 +5,36 @@ namespace App\Http\Controllers\Admin;
 use App\Charts\ReportsChart;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Repositories\Contracts\ReportsRepositoryInterface;
+
 
 class ReportsController extends Controller
 {
+    private $repository;
+
+    public function __construct(ReportsRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function months(ReportsChart $chart)
     {
+
+        $chart->labels(['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL']);
+        $chart->dataset('2018','bar', $this->repository->byMonths(2018))
+        ->options([
+            'backgroundColor' => '#111',
+        ]);
+
+
         $chart->labels(['JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL']);
         $chart->dataset('2019','bar',[
             12,14,38,22,47,56,45
         ])
         ->options([
-            'backgroundColor' => '#34e345',
+            'backgroundColor' => '#999',
         ]);
+
 
         return view('admin.charts.chart', compact('chart'));
 
